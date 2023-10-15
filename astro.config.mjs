54d4f5efd6086dec9c21565af0426cbaeafb2433
Brody <hello@brody.com>
2023-10-15 19:08:07 +1100

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,21 @@ import mdx from "@astrojs/mdx";
 import { SITE } from "./src/config";
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  integrations: [
-    tailwind({
-      config: { applyBaseStyles: false },
-    }),
-    mdx(),
-    react(),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), mdx(), react()],
   vite: {
     optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
-    },
+      exclude: ["@resvg/resvg-js"]
+    }
   },
+  output: "server",
+  adapter: netlify()
 });
