f1dd3dffc462adca70aee8411bd2417e8587a7bb
Brody <hello@brody.com>
2023-10-20 20:33:38 +1100

    add GA

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,14 @@
-import type { Site, SocialObjects } from './types';
+import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: 'https://brody.com/',
-  author: 'Brody MacLean',
-  desc: 'Design system lead & multi-disciplinary designer from Sydney, Australia.',
-  title: 'Brody MacLean',
-  ogImage: 'defaultOG.jpg',
+  website: "https://brody.com/",
+  author: "Brody MacLean",
+  desc: "Design system lead & multi-disciplinary designer from Sydney, Australia.",
+  title: "Brody MacLean",
+  ogImage: "defaultOG.jpg",
   lightAndDarkMode: true,
   postPerPage: 999,
+  analytics: "UA-60603528-2",
 };
 
 export const LOGO_IMAGE = {
@@ -19,44 +20,44 @@ export const LOGO_IMAGE = {
 
 export const SOCIALS: SocialObjects = [
   {
-    name: 'Github',
-    href: 'https://github.com/brody/',
+    name: "Github",
+    href: "https://github.com/brody/",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
   {
-    name: 'Facebook',
-    href: 'https://www.facebook.com/BrodyMacLean/',
+    name: "Facebook",
+    href: "https://www.facebook.com/BrodyMacLean/",
     linkTitle: `${SITE.title} on Facebook`,
     active: true,
   },
   {
-    name: 'Instagram',
-    href: 'https://www.instagram.com/brody/',
+    name: "Instagram",
+    href: "https://www.instagram.com/brody/",
     linkTitle: `${SITE.title} on Instagram`,
     active: true,
   },
   {
-    name: 'LinkedIn',
-    href: 'https://www.linkedin.com/in/brodymaclean/',
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/brodymaclean/",
     linkTitle: `${SITE.title} on LinkedIn`,
     active: true,
   },
   {
-    name: 'Mail',
-    href: 'mailto:hello@brody.com',
+    name: "Mail",
+    href: "mailto:hello@brody.com",
     linkTitle: `Send an email to ${SITE.title}`,
     active: false,
   },
   {
-    name: 'Twitter',
-    href: 'https://twitter.com/BrodyMaclean',
+    name: "Twitter",
+    href: "https://twitter.com/BrodyMaclean",
     linkTitle: `${SITE.title} on Twitter`,
     active: false,
   },
   {
-    name: 'CodePen',
-    href: 'https://codepen.io/brody',
+    name: "CodePen",
+    href: "https://codepen.io/brody",
     linkTitle: `${SITE.title} on CodePen`,
     active: false,
   },
