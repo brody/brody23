2deb48f45bcb81cf2f62ae9e90a6ac457279e5e3
Brody <hello@brody.com>
2023-10-21 11:31:36 +1100

    add filter for scheduledposts

--- src/utils/getSortedPosts.ts
+++ src/utils/getSortedPosts.ts
@@ -1,12 +1,16 @@
-import type { CollectionEntry } from 'astro:content';
+import type { CollectionEntry } from "astro:content";
+const now = Date.now();
 
-const getSortedPosts = (posts: CollectionEntry<'journal'>[]) =>
+const getSortedPosts = (posts: CollectionEntry<"journal">[]) =>
   posts
-    .filter(({ data }) => !data.draft)
+    .filter(
+      ({ data }) => !data.draft && data.pubDatetime.getTime() <= Date.now(),
+    )
+
     .sort(
       (a, b) =>
         Math.floor(new Date(b.data.pubDatetime).getTime() / 1000) -
-        Math.floor(new Date(a.data.pubDatetime).getTime() / 1000)
+        Math.floor(new Date(a.data.pubDatetime).getTime() / 1000),
     );
 
 export default getSortedPosts;
