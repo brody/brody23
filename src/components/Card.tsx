830f12b1eed33c3e2fa4d22c5e1ec241db0bc58d
Brody <hello@brody.com>
2023-11-01 08:19:29 +1100

    improve wide-videos, improve semantic structure

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,64 +1,44 @@
 // import Date from './Date.astro';
-import type { BlogFrontmatter } from "@content/_schemas";
-import { format } from "date-fns";
+import type { BlogFrontmatter } from '@content/_schemas'
+import { format } from 'date-fns'
 
 export interface Props {
-  href?: string;
-  frontmatter: BlogFrontmatter;
-  secHeading?: boolean;
+	href?: string
+	frontmatter: BlogFrontmatter
+	secHeading?: boolean
 }
-let currentYear = "";
+let currentYear = ''
 
 export default function Card({ href, frontmatter, secHeading = true }: Props) {
-  const { title, pubDatetime, description } = frontmatter;
+	const { title, pubDatetime, description } = frontmatter
 
-  return (
-    <>
-      <>
-        {format(new Date(pubDatetime), "yyyy") === currentYear ? (
-          ""
-        ) : (
-          <h2 className="!text-2xl !text-accent">
-            {(currentYear = format(new Date(pubDatetime), "yyyy"))}
-          </h2>
-        )}
-      </>
+	return (
+		<>
+			<>
+				{format(new Date(pubDatetime), 'yyyy') === currentYear ? (
+					''
+				) : (
+					<h2 className='h2 prose mt-12 !text-2xl !text-accent'>
+						{(currentYear = format(new Date(pubDatetime), 'yyyy'))}
+					</h2>
+				)}
+			</>
 
-      <div className="not-prose my-5">
-        <a
-          href={href}
-          className="flex flex-row !no-underline hover:text-accent"
-        >
-          {/* <div> */}
-          {secHeading ? (
-            <h2 className="my-0 whitespace-nowrap font-body text-lg leading-[140%] text-heading hover:text-accent">
-              <span className="mr-1.5 whitespace-normal">{title}</span>
-              &nbsp;
-              {/* <Datetime datetime={pubDatetime} /> */}
-              {/* <Date date={pubDatetime} dateFormat='dd LLL' /> */}
-              <span
-                className={`inline-block font-code text-sm font-normal text-subtle`}
-              >
-                {format(new Date(pubDatetime), "dd LLL")}
-              </span>
-            </h2>
-          ) : (
-            <h3 className="my-0 whitespace-nowrap font-body text-base leading-[140%] text-heading hover:text-accent">
-              <span className="mr-1.5 whitespace-normal">{title}</span>
-              &nbsp;
-              {/* <Datetime datetime={pubDatetime} /> */}
-              {/* <Date date={pubDatetime} dateFormat='dd LLL' /> */}
-              <span
-                className={`inline-block font-code text-sm font-normal text-subtle`}
-              >
-                {format(new Date(pubDatetime), "dd LLL")}
-              </span>
-            </h3>
-          )}
-          {/* </div> */}
-        </a>
-        {/* <p className='text-subtle'>{description}</p> */}
-      </div>
-    </>
-  );
+			<div className='not-prose my-5'>
+				<a href={href} className='flex flex-row !no-underline hover:text-accent'>
+					<h3 className='my-0 whitespace-nowrap font-body text-lg leading-[140%] text-heading hover:text-accent'>
+						<span className='mr-1.5 whitespace-normal'>{title}</span>
+						&nbsp;
+						<time
+							dateTime={format(new Date(pubDatetime), 'yyyy-MM-dd')}
+							className={`inline-block font-code text-sm font-normal text-subtle`}
+						>
+							{format(new Date(pubDatetime), 'dd LLL')}
+						</time>
+					</h3>
+				</a>
+				{/* <p className='text-subtle'>{description}</p> */}
+			</div>
+		</>
+	)
 }
--- src/components/GardenItem.tsx
+++ src/components/GardenItem.tsx
@@ -1,28 +1,32 @@
 // import Date from './Date.astro';
-import type { GardenFrontmatter } from '@content/_schemas';
-import { format, parseISO } from 'date-fns';
+import type { GardenFrontmatter } from '@content/_schemas'
+import { format, parseISO } from 'date-fns'
 
 export interface Props {
-  href?: string;
-  frontmatter: GardenFrontmatter;
-  secHeading?: boolean;
+	href?: string
+	frontmatter: GardenFrontmatter
+	secHeading?: boolean
 }
-let currentYear = '';
+let currentYear = ''
 
 export default function GardenItem({ href, frontmatter, secHeading = true }: Props) {
-  const { title, pubDatetime, description } = frontmatter;
+	const { title, pubDatetime, description } = frontmatter
 
-  return (
-    <div className='my-5 not-prose'>
-      <a href={href} className='flex flex-col !no-underline hover:text-accent'>
-        {/* <div> */}
-        {secHeading ? (
-          <h2 className='my-0 text-lg text-heading hover:text-accent font-body leading-[140%]'>{title}</h2>
-        ) : (
-          <h3 className=' my-0 text-base text-heading hover:text-accent font-body leading-[140%]'>{title}</h3>
-        )}
-        <p className='text-sm text-subtle'>{description}</p>
-      </a>
-    </div>
-  );
+	return (
+		<div className='my-5'>
+			<a href={href} className='flex flex-col !no-underline hover:text-accent'>
+				{/* <div> */}
+				{secHeading ? (
+					<h2 className='my-0 font-body text-lg leading-[140%] text-heading hover:text-accent'>
+						{title}
+					</h2>
+				) : (
+					<h3 className=' my-0 font-body text-base leading-[140%] text-heading hover:text-accent'>
+						{title}
+					</h3>
+				)}
+				<p className='text-sm text-subtle'>{description}</p>
+			</a>
+		</div>
+	)
 }
