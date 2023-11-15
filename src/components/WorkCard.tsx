e2405d56b469992474cde1ae2fec8eee47d387cc
Brody <hello@brody.com>
2023-11-16 00:22:38 +1100

    salt case study

--- src/components/WorkCard.tsx
+++ src/components/WorkCard.tsx
@@ -1,29 +1,39 @@
-import type { WorkFrontmatter } from "@content/_schemas";
-import { Image } from "astro:assets";
+import type { WorkFrontmatter } from '@content/_schemas'
+import { Image } from 'astro:assets'
 
 export interface Props {
-  href?: string;
-  frontmatter: WorkFrontmatter;
+	href?: string
+	frontmatter: WorkFrontmatter
 }
 
 export default function WorkCard({ href, frontmatter }: Props) {
-  const { title, description, featuredImage } = frontmatter;
-  return (
-    // <>
-    <div className="not-prose mb-16 mt-12">
-      <a href={href} className="flex flex-col !no-underline hover:text-accent">
-        <img src={featuredImage} alt="" className="wide mb-5" />
-        {/* <Image
+	const { title, description, featuredImage, tags } = frontmatter
+	return (
+		// <>
+		<div className='not-prose mb-16 mt-12'>
+			<a href={href} className='flex flex-col !no-underline hover:text-accent'>
+				<img src={featuredImage} alt='' className='wide mb-5' />
+				{/* <Image
             src={featuredImage}
             alt="Kindle with Reinventing organizations cover"
             className="wide"
           /> */}
-        <h2 className="h2 whitespace-nowrap hover:text-accent">
-          <span className="whitespace-normal">{title}</span>
-        </h2>
-        <p className="leading-normal text-subtle">{description}</p>
-      </a>
-    </div>
-    // </>
-  );
+
+				<h2 className='h2 whitespace-nowrap hover:text-accent'>
+					<span className='whitespace-normal'>{title}</span>
+				</h2>
+
+				<div className='mb-2 flex flex-row flex-wrap gap-2'>
+					{tags.map((tags) => (
+						<span className='rounded border border-default bg-subtle px-1.5 text-sm text-subtle'>
+							{tags}
+						</span>
+					))}
+				</div>
+
+				<p className='leading-normal text-subtle'>{description}</p>
+			</a>
+		</div>
+		// </>
+	)
 }
--- src/content/_schemas.ts
+++ src/content/_schemas.ts
@@ -1,44 +1,45 @@
-import { z } from "astro:content";
+import { z } from 'astro:content'
 
 export const journalSchema = z
-  .object({
-    pubDatetime: z.date(),
-    title: z.string(),
-    entrySlug: z.string().optional(),
-    featured: z.boolean().optional(),
-    draft: z.boolean().optional(),
-    tags: z.array(z.string()).default(["others"]),
-    ogImage: z.string().optional(),
-    description: z.string(),
-  })
-  .strict();
+	.object({
+		pubDatetime: z.date(),
+		title: z.string(),
+		entrySlug: z.string().optional(),
+		featured: z.boolean().optional(),
+		draft: z.boolean().optional(),
+		tags: z.array(z.string()).default(['others']),
+		ogImage: z.string().optional(),
+		description: z.string(),
+	})
+	.strict()
 
 export const gardenSchema = z
-  .object({
-    pubDatetime: z.date(),
-    title: z.string(),
-    entrySlug: z.string().optional(),
-    featured: z.boolean().optional(),
-    draft: z.boolean().optional(),
-    tags: z.array(z.string()).default(["others"]),
-    ogImage: z.string().optional(),
-    description: z.string().optional(),
-  })
-  .strict();
+	.object({
+		pubDatetime: z.date(),
+		title: z.string(),
+		entrySlug: z.string().optional(),
+		featured: z.boolean().optional(),
+		draft: z.boolean().optional(),
+		tags: z.array(z.string()).default(['others']),
+		ogImage: z.string().optional(),
+		description: z.string().optional(),
+	})
+	.strict()
 
 export const workSchema = z
-  .object({
-    pubDatetime: z.date(),
-    title: z.string(),
-    entrySlug: z.string().optional(),
-    featured: z.boolean().optional(),
-    draft: z.boolean().optional(),
-    ogImage: z.string().optional(),
-    description: z.string(),
-    featuredImage: z.string().optional(),
-  })
-  .strict();
+	.object({
+		pubDatetime: z.date(),
+		title: z.string(),
+		entrySlug: z.string().optional(),
+		featured: z.boolean().optional(),
+		draft: z.boolean().optional(),
+		ogImage: z.string().optional(),
+		description: z.string(),
+		featuredImage: z.string().optional(),
+		tags: z.array(z.string()).default(['others']),
+	})
+	.strict()
 
-export type BlogFrontmatter = z.infer<typeof journalSchema>;
-export type GardenFrontmatter = z.infer<typeof gardenSchema>;
-export type WorkFrontmatter = z.infer<typeof workSchema>;
+export type BlogFrontmatter = z.infer<typeof journalSchema>
+export type GardenFrontmatter = z.infer<typeof gardenSchema>
+export type WorkFrontmatter = z.infer<typeof workSchema>
