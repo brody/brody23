8189e7a8be57c244c0dbe6539ed98779b8f2076a
Brody MacLean <hello@brody.com>
2025-09-01 10:07:02 +1000

    update role

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,18 +1,18 @@
 // prettier.config.js
 module.exports = {
-  plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-astro"],
-  printWidth: 100,
-  semi: false,
-  singleQuote: true,
-  jsxSingleQuote: true,
-  tabWidth: 2,
-  useTabs: true,
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
-  ],
-};
+	plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+	printWidth: 100,
+	semi: false,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	tabWidth: 2,
+	useTabs: true,
+	overrides: [
+		{
+			files: '*.astro',
+			options: {
+				parser: 'astro',
+			},
+		},
+	],
+}
